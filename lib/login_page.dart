f1cd4d362b7a2f89647215df2e7685ce6293f94a
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-02-23 20:19:59 -0400

    Updated login page with a reveal password toggle

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,91 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_graveyard_frontend/dashboard_page.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   LoginPageState createState() => LoginPageState();
 }
+
 class LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController =
+  TextEditingController();
+  final TextEditingController _passwordController =
+  TextEditingController();
+
+  bool _obscurePassword = true;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscurePassword = !_obscurePassword;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final logo = Padding(
       padding: const EdgeInsets.all(20),
       child: Hero(
-          tag: 'hero',
-          child: CircleAvatar(
-            radius: 56.0,
-            child: Image.asset('assets/ac-logo.png'),
-          )
-      ),
-    );
-    final inputEmail = Padding(
-      padding: const EdgeInsets.only(bottom: 10),
-      child: TextField(
-        keyboardType: TextInputType.emailAddress,
-        decoration: InputDecoration(
-            hintText: 'Email',
-            contentPadding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(50.0)
-            )
-        ),
-      ),
-    );
-    final inputPassword = Padding(
-      padding: const EdgeInsets.only(bottom: 20),
-      child: TextField(
-        keyboardType: TextInputType.emailAddress,
-        obscureText: true,
-        decoration: InputDecoration(
-            hintText: 'Password',
-            contentPadding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(50.0)
-            )
-        ),
-      ),
-    );
-    final buttonLogin = Padding(
-      padding: const EdgeInsets.only(bottom: 5),
-      child: ButtonTheme(
-        height: 56,
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.black87,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(50),
-            ),
-          ),
-          onPressed: () => {},
-          child: const Text('Login', style: TextStyle(color: Colors.white, fontSize: 20)),
+        tag: 'hero',
+        child: CircleAvatar(
+          radius: 56.0,
+          child: Image.asset('assets/ac-logo.png'),
         ),
       ),
     );
 
-    const buttonForgotPassword = TextButton(
-      onPressed: null,
-      child: Text(
-        'Forgot Password',
-        style: TextStyle(color: Colors.grey, fontSize: 16),
+    final username = TextFormField(
+      controller: _usernameController,
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+        hintText: 'Username',
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
       ),
     );
-    return SafeArea(
-        child: Scaffold(
-          body: Center(
-            child: ListView(
-              shrinkWrap: true,
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              children: <Widget>[
-                logo,
-                inputEmail,
-                inputPassword,
-                buttonLogin,
-                buttonForgotPassword
-              ],
+
+    final password = TextFormField(
+      controller: _passwordController,
+      autofocus: false,
+      obscureText: _obscurePassword,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+        suffixIcon: IconButton(
+          icon: Icon(
+            _obscurePassword ? Icons.visibility_off : Icons.visibility,
+          ),
+          onPressed: _togglePasswordVisibility,
+        ),
+      ),
+    );
+
+    final loginButton = Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const DashboardPage()),
+          );
+        },
+        child: const Text('Log In'),
+      ),
+    );
+
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  logo,
+                  const SizedBox(height: 48.0),
+                  username,
+                  const SizedBox(height: 8.0),
+                  password,
+                  const SizedBox(height: 24.0),
+                  loginButton,
+                ],
+              ),
             ),
           ),
-        )
+        ),
+      ),
     );
   }
 }
