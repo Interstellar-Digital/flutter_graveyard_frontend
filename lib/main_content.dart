5f5bd1eb69e19bc09e687adec6a0c26d5192b5e2
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-02-25 01:43:52 -0400

    Added logic to view details on select

--- lib/main_content.dart
+++ lib/main_content.dart
@@ -1,62 +1,10 @@
 import 'package:flutter/material.dart';
+import 'graves.dart';
+import 'grave_details_page.dart';
 
 
 class MainContent extends StatelessWidget {
-  // Dummy data for a list of graves
-  final List<Map<String, dynamic>> graves = [
-    {
-      'id': '1',
-      'grave_name': 'John Doe',
-      'status': 'occupied',
-    },
-    {
-      'id': '2',
-      'grave_name': 'Jane Doe',
-      'status': 'unoccupied',
-    },
-    {
-      'id': '3',
-      'grave_name': 'Alice Smith',
-      'status': 'occupied',
-    },
-    {
-      'id': '4',
-      'grave_name': 'Bob Johnson',
-      'status': 'unoccupied',
-    },
-    {
-      'id': '5',
-      'grave_name': 'David Lee',
-      'status': 'occupied',
-    },
-    {
-      'id': '6',
-      'grave_name': 'Mary Davis',
-      'status': 'unoccupied',
-    },
-    {
-      'id': '7',
-      'grave_name': 'Tom Brown',
-      'status': 'occupied',
-    },
-    {
-      'id': '8',
-      'grave_name': 'Sara Wilson',
-      'status': 'unoccupied',
-    },
-    {
-      'id': '9',
-      'grave_name': 'Mike Jackson',
-      'status': 'occupied',
-    },
-    {
-      'id': '10',
-      'grave_name': 'Jenny Lopez',
-      'status': 'unoccupied',
-    },
-  ];
-
-  MainContent({super.key});
+  const MainContent({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -82,10 +30,20 @@ class MainContent extends StatelessWidget {
       itemBuilder: (BuildContext context, int index) {
         final grave = graves[index];
 
-        return Card(
-          child: ListTile(
-            title: Text(grave['grave_name']),
-            subtitle: Text('ID: ${grave['id']}, Status: ${grave['status']}'),
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => GraveDetailsPage(grave: grave),
+              ),
+            );
+          },
+          child: Card(
+            child: ListTile(
+              title: Text(grave['grave_name']),
+              subtitle: Text('ID: ${grave['id']}, Status: ${grave['status']}'),
+            ),
           ),
         );
       },
