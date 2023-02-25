90c452c1de6076f9c0cbf21db971e7056af93d8e
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-02-25 01:42:40 -0400

    Updated dummy data to a separate file to import to other files

---
+++ lib/graves.dart
@@ -0,0 +1,52 @@
+final List<Map<String, dynamic>> graves = [
+  {
+    'id': '1',
+    'grave_name': 'John Doe',
+    'status': 'occupied',
+  },
+  {
+    'id': '2',
+    'grave_name': 'Jane Doe',
+    'status': 'unoccupied',
+  },
+  {
+    'id': '3',
+    'grave_name': 'Alice Smith',
+    'status': 'occupied',
+  },
+  {
+    'id': '4',
+    'grave_name': 'Bob Johnson',
+    'status': 'unoccupied',
+  },
+  {
+    'id': '5',
+    'grave_name': 'David Lee',
+    'status': 'occupied',
+  },
+  {
+    'id': '6',
+    'grave_name': 'Mary Davis',
+    'status': 'unoccupied',
+  },
+  {
+    'id': '7',
+    'grave_name': 'Tom Brown',
+    'status': 'occupied',
+  },
+  {
+    'id': '8',
+    'grave_name': 'Sara Wilson',
+    'status': 'unoccupied',
+  },
+  {
+    'id': '9',
+    'grave_name': 'Mike Jackson',
+    'status': 'occupied',
+  },
+  {
+    'id': '10',
+    'grave_name': 'Jenny Lopez',
+    'status': 'unoccupied',
+  },
+];
