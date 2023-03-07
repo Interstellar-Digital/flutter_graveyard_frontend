bfe5262b59caa4894a3fe014b4135f2be275091b
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-03-06 22:04:14 -0400

    add: repository for data and data models

---
+++ lib/data_models.dart
@@ -0,0 +1,91 @@
+class User {
+  final String userID;
+  final String username;
+  final String password;
+  final String role;
+
+  User({
+    required this.userID,
+    required this.username,
+    required this.password,
+    required this.role,
+  });
+}
+
+class Graveyard {
+  final String graveyardID;
+  final String name;
+  final String location;
+  final int numberOfPlots;
+
+  Graveyard({
+    required this.graveyardID,
+    required this.name,
+    required this.location,
+    required this.numberOfPlots,
+  });
+}
+
+class Grave {
+  final String graveID;
+  final int plotNumber;
+  final String lastBuried;
+  final String nextAvailable;
+
+  Grave({
+    required this.graveID,
+    required this.plotNumber,
+    required this.lastBuried,
+    required this.nextAvailable,
+  });
+}
+
+class Deceased {
+  final String deceasedID;
+  final String name;
+  final String dateOfDeath;
+  final String causeOfDeath;
+  final String plotID;
+
+  Deceased({
+    required this.deceasedID,
+    required this.name,
+    required this.dateOfDeath,
+    required this.causeOfDeath,
+    required this.plotID,
+  });
+}
+
+class Reservation {
+  final String reservationID;
+  final String plotID;
+  final String userID;
+  final String date;
+
+  Reservation({
+    required this.reservationID,
+    required this.plotID,
+    required this.userID,
+    required this.date,
+  });
+}
+
+class BillingInformation {
+  final String billingID;
+  final String userID;
+  final double amount;
+  final String date;
+  final String billingAddress;
+  final String creditCardNumber;
+  final String expirationDate;
+
+  BillingInformation({
+    required this.billingID,
+    required this.userID,
+    required this.amount,
+    required this.date,
+    required this.billingAddress,
+    required this.creditCardNumber,
+    required this.expirationDate,
+  });
+}
---
+++ lib/data_repository.dart
@@ -0,0 +1,81 @@
+import 'package:flutter_graveyard_frontend/data_models.dart';
+class UserRepository {
+  Future<User?> getUserById(int userId) async {
+    // code to retrieve a user with the given id from the database
+    return null;
+  }
+
+  Future<User?> getUserByUsernameAndPassword(String username, String password) async {
+    // code to retrieve a user with the given username and password from the database
+
+    return null;
+  }
+
+  Future<void> saveUser(User user) async {
+    // code to save the user to the database
+  }
+
+  Future<void> deleteUser(int userId) async {
+    // code to delete the user with the given id from the database
+  }
+}
+
+class GraveyardRepository {
+  Future<List<Graveyard>?> getAllGraveyards() async {
+    // code to retrieve all graveyards from the database
+    return null;
+  }
+
+  Future<Graveyard?> getGraveyardById(int graveyardId) async {
+    // code to retrieve a graveyard with the given id from the database
+    return null;
+  }
+
+  Future<void> saveGraveyard(Graveyard graveyard) async {
+    // code to save the graveyard to the database
+  }
+
+  Future<void> deleteGraveyard(int graveyardId) async {
+    // code to delete the graveyard with the given id from the database
+  }
+}
+
+class GraveRepository {
+  Future<List<Grave>?> getGravesByGraveyardId(int graveyardId) async {
+    // code to retrieve all graves for the graveyard with the given id from the database
+    return null;
+  }
+
+  Future<Grave?> getGraveById(int graveId) async {
+    // code to retrieve a grave with the given id from the database
+    return null;
+  }
+
+  Future<void> saveGrave(Grave grave) async {
+    // code to save the grave to the database
+  }
+
+  Future<void> deleteGrave(int graveId) async {
+    // code to delete the grave with the given id from the database
+  }
+}
+
+class DeceasedRepository {
+  Future<List<Deceased>?> getDeceasedByGraveId(int graveId) async {
+    // code to retrieve all deceased for the grave with the given id from the database
+    return null;
+  }
+
+  Future<Deceased?> getDeceasedById(int deceasedId) async {
+    // code to retrieve a deceased with the given id from the database
+    return null;
+  }
+
+  Future<void> saveDeceased(Deceased deceased) async {
+    // code to save the deceased to the database
+  }
+
+  Future<void> deleteDeceased(int deceasedId) async {
+    // code to delete the deceased with the given id from the database
+  }
+}
