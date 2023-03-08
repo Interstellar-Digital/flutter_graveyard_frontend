ed3483d55de6a2560f56bb06016eaef511f18ea8
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-03-07 23:16:41 -0400

    Add: a delete and add button with changed layout to 3 columns

--- lib/graveyard_list.dart
+++ lib/graveyard_list.dart
@@ -10,61 +10,81 @@ class GraveyardSelectionPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const NavBar(),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              childAspectRatio: 1.0,
-              crossAxisSpacing: 10.0,
-              mainAxisSpacing: 10.0,
+      body: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    // TODO: Implement add graveyard functionality
+                  },
+                  icon: const Icon(Icons.add),
+                  label: const Text('Add Graveyard'),
+                ),
+              ],
             ),
-            itemCount: graveyards.length,
-            itemBuilder: (BuildContext context, int index) {
-              final graveyard = graveyards[index];
-              return Card(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: GridView.builder(
+                gridDelegate:
+                const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.0,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0,
+                ),
+                itemCount: graveyards.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final graveyard = graveyards[index];
+                  return Card(
+                    child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        IconButton(
-                          icon: const Icon(Icons.delete),
-                          onPressed: () {
-                            //TODO: implement delete functionality
-                          },
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.add),
-                          onPressed: () {
-                            //TODO: implement add functionality
-                          },
+                        Expanded(
+                          child: ListTile(
+                            title: Text(graveyard['name']),
+                            subtitle: Text(graveyard['location']),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => MainContent(
+                                    selectedGraveyard: graveyard,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
                         ),
                       ],
                     ),
-                    Expanded(
-                      child: ListTile(
-                        title: Text(graveyard['name']),
-                        subtitle: Text(graveyard['location']),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  MainContent(selectedGraveyard: graveyard),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
+                  );
+                },
+              ),
+            ),
           ),
-        ),
+          Expanded(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    // TODO: Implement add graveyard functionality
+                  },
+                  icon: const Icon(Icons.delete),
+                  label: const Text('Delete Graveyard'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
