404f34ed6d35aea2c27a75f057c592e05275fe10
WarrenShanghie <wshanghie@gmail.com>
2023-03-24 04:32:04 -0400

    removed Graves from data_models.dart to its own file

--- lib/data_models.dart
+++ lib/data_models.dart
@@ -1,18 +1,6 @@
 
 
-class Grave {
-  final String graveID;
-  final int plotNumber;
-  final String lastBuried;
-  final String nextAvailable;
 
-  Grave({
-    required this.graveID,
-    required this.plotNumber,
-    required this.lastBuried,
-    required this.nextAvailable,
-  });
-}
 
 class Deceased {
   final String deceasedID;
