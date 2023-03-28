4f754d0c9acff09336d51ebefbd022feb7738176
WarrenShanghie <wshanghie@gmail.com>
2023-03-28 15:06:02 -0400

    login page completed with no functionallity

---
+++ lib/login/login_functionallity.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_graveyard_frontend/graveyardSelection/graveyardSelectionSreen.dart';
+
+class LoginDetails extends StatelessWidget {
+  const LoginDetails({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 400,
+      width: 500,
+      decoration: BoxDecoration(
+          color: const Color.fromRGBO(192, 224, 229, 1),
+          borderRadius: BorderRadius.circular(20)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline,
+                  ),
+                )),
+            TextFormField(
+              //username
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                hintText: 'Username',
+                contentPadding:
+                    const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(32.0),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+                //password
+                decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              hintText: 'Password',
+              contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(32.0),
+              ),
+            )),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: ElevatedButton(
+                //login button
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(
+                      Color.fromRGBO(147, 198, 231, 1)),
+                ),
+                onPressed: () {
+                  //navigate to selectgraveyard_screen
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          const GraveyardInDistrictSelectionScreen(),
+                    ),
+                  );
+                },
+                child: const Text('Log In'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/login/login_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'login_functionallity.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(100),
+          child: Container(
+            color: const Color.fromRGBO(174, 226, 255, 1),
+            child: Row(
+              children: [
+                SizedBox(
+                  child: Image.network(
+                    'https://raw.githubusercontent.com/Interstellar-Digital/images/main/images/gp_logo.png',
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: const Center(child: LoginDetails())
+      ),
+    );
+  }
+}
