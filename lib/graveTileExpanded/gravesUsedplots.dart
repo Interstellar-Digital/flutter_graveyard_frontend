5ce83a0d33bcbef1a38c69c1740c7e4b4882e574
WarrenShanghie <wshanghie@gmail.com>
2023-04-04 09:16:15 -0400

    inital layout

---
+++ lib/graveTileExpanded/gravesUsedplots.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AddGravePlot extends StatelessWidget {
+  const AddGravePlot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Text('Available Plots'),
+          Divider(),
+          Row(
+            children: [
+              Text('Single'),
+              Text('SingleHolder'),
+            ],
+          ),
+          Divider(),
+          Row(
+            children: [Text('Companion'), Text('Number')],
+          ),
+          Divider(),
+          Row(
+            children: [
+              Text('Family'),
+              Text('Number'),
+            ],
+          ),
+          Divider(),
+          Row(
+            children: [
+              Text('Cremated Remains'),
+              Text('Number'),
+            ],
+          ),
+          Divider(),
+          Row(
+            children: [
+              Text('Total'),
+              Text('Number'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
