4a7e39f0ecb574fb68a242bb25b8cd4f8324a96c
WarrenShanghie <wshanghie@gmail.com>
2023-04-06 02:50:23 -0400

    add burial pop up without buttons and functionality

--- lib/burialTileExpanded/addBurial.dart
+++ lib/burialTileExpanded/addBurial.dart
@@ -1,6 +1,170 @@
 import 'package:flutter/material.dart';
 
-FloatingActionButton AddGrave() {
+class AddDeceasePopup extends StatelessWidget {
+  const AddDeceasePopup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+        title: Text(
+          'Add deceased',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        content: SizedBox(
+          height: 700,
+          width: 800,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text('Please fill in the information below'),
+                Text("Must fill in the information marked with the asteric '*'"),
+                SizedBox(height: 30,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: 220,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Deceased First Name *',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 220,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Deceased Middle Name',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 220,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Deceased Last Name *',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: 220,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Deceased Date of Birth *',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 220,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Deceased Date of Death *',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 220,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Deceased Address',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: 220,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Plot Type *',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 220,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Plot Number *',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 220,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Cemetery Location *',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: 220,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Next of Kin *',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 220,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Next of Kin Contact *',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 220,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Next of Kin Email *',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+FloatingActionButton AddGrave(BuildContext context) {
   return FloatingActionButton(
     backgroundColor: Color.fromRGBO(6, 208, 111, 1),
     child: Text(
@@ -9,6 +173,11 @@ FloatingActionButton AddGrave() {
       style: TextStyle(color: Colors.black),
     ),
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-    onPressed: () {}, //add person to grave list
+    onPressed: () {
+      showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AddDeceasePopup(),
+      );
+    }, //add person to grave list
   );
 }
