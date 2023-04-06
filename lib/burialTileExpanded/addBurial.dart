85b2771593751e4b1af4a073b93a5b0778765e9c
WarrenShanghie <wshanghie@gmail.com>
2023-04-06 02:58:41 -0400

    buttons added

--- lib/burialTileExpanded/addBurial.dart
+++ lib/burialTileExpanded/addBurial.dart
@@ -6,161 +6,182 @@ class AddDeceasePopup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-        title: Text(
-          'Add deceased',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-        content: SizedBox(
-          height: 700,
-          width: 800,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Text('Please fill in the information below'),
-                Text("Must fill in the information marked with the asteric '*'"),
-                SizedBox(height: 30,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SizedBox(
-                      width: 220,
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Deceased First Name *',
-                        ),
+      title: Text(
+        'Add deceased',
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+      content: SizedBox(
+        height: 400,
+        width: 800,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text('Please fill in the information below'),
+              Text("Must fill in the information marked with the asteric '*'"),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 220,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Deceased First Name *',
                       ),
                     ),
-                    SizedBox(
-                      width: 220,
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Deceased Middle Name',
-                        ),
+                  ),
+                  SizedBox(
+                    width: 220,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Deceased Middle Name',
                       ),
                     ),
-                    SizedBox(
-                      width: 220,
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Deceased Last Name *',
-                        ),
+                  ),
+                  SizedBox(
+                    width: 220,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Deceased Last Name *',
                       ),
                     ),
-                  ],
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SizedBox(
-                      width: 220,
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Deceased Date of Birth *',
-                        ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 220,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Deceased Date of Birth *',
                       ),
                     ),
-                    SizedBox(
-                      width: 220,
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Deceased Date of Death *',
-                        ),
+                  ),
+                  SizedBox(
+                    width: 220,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Deceased Date of Death *',
                       ),
                     ),
-                    SizedBox(
-                      width: 220,
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Deceased Address',
-                        ),
+                  ),
+                  SizedBox(
+                    width: 220,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Deceased Address',
                       ),
                     ),
-                  ],
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SizedBox(
-                      width: 220,
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Plot Type *',
-                        ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 220,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Plot Type *',
                       ),
                     ),
-                    SizedBox(
-                      width: 220,
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Plot Number *',
-                        ),
+                  ),
+                  SizedBox(
+                    width: 220,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Plot Number *',
                       ),
                     ),
-                    SizedBox(
-                      width: 220,
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Cemetery Location *',
-                        ),
+                  ),
+                  SizedBox(
+                    width: 220,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Cemetery Location *',
                       ),
                     ),
-                  ],
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SizedBox(
-                      width: 220,
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Next of Kin *',
-                        ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 220,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Next of Kin *',
                       ),
                     ),
-                    SizedBox(
-                      width: 220,
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Next of Kin Contact *',
-                        ),
+                  ),
+                  SizedBox(
+                    width: 220,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Next of Kin Contact *',
                       ),
                     ),
-                    SizedBox(
-                      width: 220,
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Next of Kin Email *',
-                        ),
+                  ),
+                  SizedBox(
+                    width: 220,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Next of Kin Email *',
                       ),
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+      actions: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(bottom: 18),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'Cancel'),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'okay'),
+                child: const Text('Add Deceased'),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
 
