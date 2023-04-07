baa4518c09c4bfa7fea55bdc3a49e762ac62c72c
WarrenShanghie <wshanghie@gmail.com>
2023-04-07 07:28:16 -0400

    profile page with no functionality

---
+++ lib/profile/adminProfile.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_graveyard_frontend/navBar/navigationBar.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: NavBar(onPressCallBack: () {}, pageTitle: 'ProfilePageHolder'),
+      body: Center(
+        child: Row(
+          children: [
+            Column(
+              children: [
+                Icon(
+                  Icons.image,
+                  size: 500,
+                ),
+                Text(
+                  'Adminstrator',
+                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 300, top: 50),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'John Doe',
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'Cemetery Adminstrator',
+                    style: TextStyle(fontSize: 20),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
