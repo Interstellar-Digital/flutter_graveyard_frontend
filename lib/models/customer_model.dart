8090ace9b1c75cf1360fc34f0be02d81a535930b
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-04-14 11:00:29 -0400

    Updated: with id field

--- lib/models/customer_model.dart
+++ lib/models/customer_model.dart
@@ -1,9 +1,11 @@
 class Customer {
+  final String id;
   final String name;
   final String email;
   final String phone;
 
   Customer({
+    required this.id,
     required this.name,
     required this.email,
     required this.phone,
@@ -11,11 +13,16 @@ class Customer {
 
   factory Customer.fromJson(Map<String, dynamic> json) {
     return Customer(
+      id: json['id'],
       name: json['name'],
       email: json['email_address'],
       phone: json['phone_number'],
     );
   }
+
+  get phoneNumber => phone;
+
+  get emailAddress => email;
 
   Map<String, dynamic> toJson() {
     return {
