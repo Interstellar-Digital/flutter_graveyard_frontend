89f092f181bb42830ecabcdbe2d99ee35e4cc48b
WarrenShanghie <wshanghie@gmail.com>
2023-04-17 18:09:15 -0400

    updated the method for the new datatable view

--- lib/burialTileExpanded/burialsScreen.dart
+++ lib/burialTileExpanded/burialsScreen.dart
@@ -10,7 +10,6 @@ class BurialScreen extends StatelessWidget {
   final String pageTitle;
   const BurialScreen({super.key, required this.pageTitle});
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +23,7 @@ class BurialScreen extends StatelessWidget {
         body: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.only(top: 20),
+              padding: const EdgeInsets.only(top: 20, bottom: 40),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -35,36 +34,6 @@ class BurialScreen extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 40),
                     child: AddGrave(context),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 40),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    'Name',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-                  ),
-                  Text('Plot Type',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
-                  Text('Plot No.',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
-                  Text('Stay',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
-                  Text('Date of Birth',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
-                  Text('Date of Death',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
-                  SizedBox(
-                    width: 50,
                   )
                 ],
               ),
