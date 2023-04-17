add9e584317517a45eb34377ac565b65128b52f9
WarrenShanghie <wshanghie@gmail.com>
2023-04-16 22:16:22 -0400

    payment method added with no functionality

--- lib/paymentTileExpaned/paymentMethod.dart
+++ lib/paymentTileExpaned/paymentMethod.dart
@@ -8,88 +8,141 @@ class PaymentMethodScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: NavBar(
-          onPressCallBack: () {},
+          onPressCallBack: () {
+            Navigator.pop(context);
+          },
           pageTitle: 'payment method',
           context: context),
-      body: Column(
-        children: [
-          Text(
-            'Please fill out the information below to make payment *',
-            style: TextStyle(
-              fontSize: 24,
-              decoration: TextDecoration.underline,
-              decorationThickness: 2,
-            ),
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: 40,
-                height: 20,
-                child: Container(color: Colors.grey, child: Text('VISA')),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20, bottom: 40),
+              child: Text(
+                'Please fill out the information below to make payment *',
+                style: TextStyle(
+                  fontSize: 24,
+                  decoration: TextDecoration.underline,
+                  decorationThickness: 2,
+                ),
               ),
-              SizedBox(
-                width: 40,
-                height: 20,
-                child: Container(color: Colors.grey, child: Text('Credit')),
-              )
-            ],
-          ),
-          Card(
-            color: Color.fromRGBO(174, 226, 255, 1),
-            child: Column(
-              children: [
-                TextFormField(
-                    decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  filled: true,
-                  fillColor: Colors.white,
-                  labelText: 'Card Number',
-                )),
-                Row(
-                  children: [
-                    SizedBox(
-                      width: 200,
+            ),
+            Container(
+              width: 700,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(174, 226, 255, 1),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black, offset: Offset(1, 2), blurRadius: 5)
+                ],
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 40,
+                        height: 20,
+                        child:
+                            Container(color: Colors.grey, child: Text('VISA')),
+                      ),
+                      SizedBox(
+                        width: 40,
+                        height: 20,
+                        child: Container(
+                            color: Colors.grey, child: Text('Credit')),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: SizedBox(
+                      width: 600,
                       child: TextFormField(
                           decoration: const InputDecoration(
                         border: OutlineInputBorder(),
                         filled: true,
                         fillColor: Colors.white,
-                        labelText: 'Expiration Date',
+                        labelText: 'Card Number',
                       )),
                     ),
-                    SizedBox(
-                      width: 200,
-                      child: TextFormField(
-                          decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        filled: true,
-                        fillColor: Colors.white,
-                        labelText: 'CVV',
-                      )),
-                    )
-                  ],
-                ),
-                TextFormField(
-                    decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  filled: true,
-                  fillColor: Colors.white,
-                  labelText: 'First Name',
-                )),
-                TextFormField(
-                    decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  filled: true,
-                  fillColor: Colors.white,
-                  labelText: 'Last Name',
-                )),
-                TextButton(onPressed: () {}, child: Text('Purchase')),
-                TextButton(onPressed: () {}, child: Text('Cancel'))
-              ],
-            ),
-          )
-        ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 300,
+                        child: TextFormField(
+                            decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          filled: true,
+                          fillColor: Colors.white,
+                          labelText: 'Expiration Date',
+                        )),
+                      ),
+                      SizedBox(
+                        width: 300,
+                        child: TextFormField(
+                            decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          filled: true,
+                          fillColor: Colors.white,
+                          labelText: 'CVV',
+                        )),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  SizedBox(
+                    width: 600,
+                    child: TextFormField(
+                        decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'First Name',
+                    )),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  SizedBox(
+                    width: 600,
+                    child: TextFormField(
+                        decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Last Name',
+                    )),
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text('Purchase',
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ))),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Cancel',
+                        style: TextStyle(
+                          color: Colors.black,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ))
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
